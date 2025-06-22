d5066160ceb5f0436d8ff8ce772508e35f2941aa
ctrixcode <coderck@proton.me>
2025-06-22 13:17:52 +0530

    Added basic mongodb file for the connection

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import dotenv from 'dotenv';
+dotenv.config();
+
 import app from './app';
 import { logger } from './utils';
-
-// Load environment variables
-dotenv.config();
+import { dbInstance } from './config';
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  logger.info(`🚀 Server is running on port ${PORT}`);
-  logger.info(`📊 Health check available at: http://localhost:${PORT}/healthz`);
-  logger.info(`🌍 Environment: ${process.env.NODE_ENV || 'development'}`);
-});
+(async () => {
+  await dbInstance();
+  app.listen(PORT, () => {
+    logger.info(`🚀 Server is running on port ${PORT}`);
+    logger.info(`📊 Health check available at: http://localhost:${PORT}/healthz`);
+    logger.info(`🌍 Environment: ${process.env.NODE_ENV || 'development'}`);
+  });
+})();
