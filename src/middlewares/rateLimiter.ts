1fd50e0812b15ab3e10195f4b790c407081e433c
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-11-04 21:24:43 +0530

    fix: rate limitter issue

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -7,5 +7,5 @@ export const generalLimiter = rateLimit({
   standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-  message: 'Too many requests, please try again later.',
+  // store: ... , // Redis, Memcached, etc. See below.
 });
